020b4a2d4b6806f2936471c7fdeb7a8919e82928
Marc <mfoores6@gmail.com>
2025-06-04 09:24:35 +0200

    stmp ionos correcto?

--- src/main/java/es/familycash/proveedores/service/EmailService.java
+++ src/main/java/es/familycash/proveedores/service/EmailService.java
@@ -1,9 +1,11 @@
 package es.familycash.proveedores.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+
+import jakarta.mail.internet.MimeMessage;
 
 @Service
 public class EmailService {
@@ -12,17 +14,23 @@ public class EmailService {
     private JavaMailSender mailSender;
 
     public void sendRecuperacionEmail(String toEmail, String token) {
-        String link = "http://proveedores.familycash.es/restablecer-password?token=" + token;
-        String subject = "Recuperación de contraseña - FamilyCash Proveedores";
-        String body = "Hola, haz clic en el siguiente enlace para restablecer tu contraseña:\n\n" + link + "\n\nEste enlace expirará en 30 minutos.";
+        try {
+            String link = "http://proveedores.familycash.es/restablecer-password?token=" + token;
+            String subject = "Recuperación de contraseña - FamilyCash Proveedores";
+            String body = "Hola, haz clic en el siguiente enlace para restablecer tu contraseña:<br><br>" +
+                    "<a href=\"" + link + "\">Restablecer contraseña</a><br><br>Este enlace expirará en 30 minutos.";
 
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(toEmail);
-        message.setSubject(subject);
-        message.setText(body);
-        mailSender.send(message);
+            MimeMessage message = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
+
+            helper.setFrom("avisosweb@familycash.es"); // 👈 ESTO ES LO QUE HACE FALTA
+            helper.setTo(toEmail);
+            helper.setSubject(subject);
+            helper.setText(body, true); // true = HTML
+
+            mailSender.send(message);
+        } catch (Exception e) {
+            System.err.println("Error al enviar el correo: " + e.getMessage());
+        }
     }
 }
-
-    
-
